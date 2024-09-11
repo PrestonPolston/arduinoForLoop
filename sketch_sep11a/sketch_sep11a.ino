1fa6862770acfa613a74fef56237d60b503cb111
preston <polston13@outlook.com>
2024-09-11 14:02:27 -0400

    init for loop

---
+++ sketch_sep11a/sketch_sep11a.ino
@@ -0,0 +1,11 @@
+// for loop to ask user how many times they want to flash RGB LED
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
